623d14f6b786f7eb1fc2dbb01de614a1d6038922
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-25 19:48:37 +0700

    routes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
-
 const app = express();
+const tasks = require("./routes/tasks");
+
+const port = 3000;
 
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
 
-const port = 3000;
+app.use("/api/v1/tasks", tasks);
 
 app.listen(port, console.log(`server is listening on port ${port}...`));
