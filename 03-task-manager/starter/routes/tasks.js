f386e77924d79cefcfee6e384de14bcc818fe5d0
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-26 08:05:46 +0700

    all routes

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,7 +1,21 @@
 const express = require("express");
-const { getAllTasks } = require("../controllers/tasks");
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 const router = express.Router();
 
 router.route("/").get(getAllTasks);
+
+router.route("/").get(createTask);
+
+router.route("/:id").get(getTask);
+
+router.route("/:id").get(updateTask);
+
+router.route("/:id").get(deleteTask);
 
 module.exports = router;
