ac7f768d09fabbaf221b0f026728280f49fd7e87
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-28 15:52:14 +0700

    mongo connection

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -13,7 +15,7 @@ app.use(express.json());
 // routes
 
 app.get("/", (req, res) => {
-  res.send('<h1>Store API</h1><a href"/api/v1/products"=>products route</a>');
+  res.send('<h1>Store API</h1><a href="/api/v1/products"=>products route</a>');
 });
 
 // products route
@@ -26,6 +28,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     //connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}`));
   } catch (error) {
     console.log(error);
