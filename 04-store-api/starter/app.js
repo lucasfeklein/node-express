d5f807672181a193f082bbb1ab87201718691cbf
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-28 11:20:00 +0700

    setup

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -3,3 +3,15 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href"/api/v1/products"=>products route</a>');
+});
