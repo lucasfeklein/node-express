1d282f68104d6f58577583290b701718b6f2332b
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-28 11:22:08 +0700

    app listen

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -15,3 +15,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href"/api/v1/products"=>products route</a>');
 });
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port, console.log(`Server is listening port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
