996c4eb5d2dabd585f5a755d4fa0a90931dd539d
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-28 09:39:34 +0700

    error message

--- 03-task-manager/starter/middleware/error-handler.js
+++ 03-task-manager/starter/middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ msg: err });
+  return res.status(500).json({ msg: `Something went wrong` });
 };
 
 module.exports = errorHandlerMiddleware;
