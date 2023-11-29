e218c9c34c010cf1522c165f9ec678f39a0cd48e
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-29 15:07:00 +0700

    dynamic find products route

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,13 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const products = await Product.find({ featured: req.query.featured });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
