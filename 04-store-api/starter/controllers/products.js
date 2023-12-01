9280644a7d495dcacbdf4dbf15950f715f61e2b5
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-01 17:21:05 +0700

    fields query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -24,12 +24,19 @@ const getAllProducts = async (req, res) => {
 
   console.log(queryObject);
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
