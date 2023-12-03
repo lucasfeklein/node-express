638986ddc86edcde3b7cd534624d7dafbe4f3eeb
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-03 11:26:33 +0700

    , instead of .

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -4,11 +4,10 @@ const login = async (req, res) => {
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200),
-    json({
-      msg: `Hello, John Doe`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber} `,
-    });
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber} `,
+  });
 };
 
 module.exports = {
