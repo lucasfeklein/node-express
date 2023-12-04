caaea3001cdfdd8a8d920e132d543437b6309921
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-04 13:24:58 +0700

    jwt secret

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -5,14 +5,16 @@ const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw new CustomAPIError("Please provide email and password");
+    throw new CustomAPIError("Please provide email and password", 400);
   }
 
   const id = new Date().getDate();
 
-  const token = jwt.sign({ id, username });
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
-  res.send("Fake Login/Register/Signup Route");
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
