c61151ff2e5e06639d97519cad4588c4ec354da0
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-13 11:23:04 -0300

    hash password in mongoose

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,6 +22,12 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minlength: 6,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
