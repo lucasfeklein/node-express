6d0ae568c589916eac5b02622aff02f5af7e8ace
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-14 11:25:27 -0300

    mongoose methods

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,15 +1,10 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SECRET,
-    { expiresIn: "30d" }
-  );
-  res.status(StatusCodes.CREATED).json({ token });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
