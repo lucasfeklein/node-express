2bc05e3f75331f64623720d652b494a707a60fc6
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-17 09:25:18 -0300

    custom errors

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -5,6 +5,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong try again later",
   };
 
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
+  }
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id ${err.value}`;
+    customError.statusCode = 404;
+  }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
